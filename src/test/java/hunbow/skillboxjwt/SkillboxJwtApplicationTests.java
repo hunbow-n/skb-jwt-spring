596bd5ec50a456662e2d6f7013fa8044d5cfd6ca
Никита Сержевский <hunbow.n@gmail.com>
2024-09-08 23:53:00 +0300

    init

---
+++ src/test/java/hunbow/skillboxjwt/SkillboxJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package hunbow.skillboxjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillboxJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
